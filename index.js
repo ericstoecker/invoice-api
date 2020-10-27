70f3e521271b1f7e9026944797e630343b4d67f8
ericstoecker <eric.stoecker@gmx.net>
2020-10-27 12:14:00 +0100

    added properties file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+//properties
+const properties = require('./src/env/development.json');
+
 //import routes
 const customersRoute = require('./src/router/customersRoute');
 const carsRoute = require('./src/router/carsRoute');
@@ -12,7 +15,9 @@ const accessRoute = require('./src/router/accessRoute');
 const middleware = require('./src/middleware/middleware');
 
 //connecting to DB
-mongoose.connect('mongodb://localhost:27017/invoice', { useNewUrlParser: true }, () => {
+const db = properties.database;
+
+mongoose.connect(`${db.type}://${db.host}:${db.port}/${db.name}`, { useNewUrlParser: true }, () => {
     console.log('connected to DB!');
 });
 
@@ -33,7 +38,9 @@ app.use('/api/access', accessRoute);
 
 
 //listen for requests
-const port = process.env.port || 8888;
+const appProperties = properties.appProperties;
+
+const port = process.env.port || Number(appProperties.port);
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
 });
